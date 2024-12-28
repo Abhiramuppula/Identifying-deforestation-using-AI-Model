6fcd0d330fd4009183baf9c91b35f5f291552e16
Abhiram <135219967+Abhiramuppula@users.noreply.github.com>
2024-12-28 14:24:55 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.5.5-10.4.28-MariaDB : Database - deforestation
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`deforestation` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `deforestation`;
+
+/*Table structure for table `ureg` */
+
+DROP TABLE IF EXISTS `ureg`;
+
+CREATE TABLE `ureg` (
+  `name` char(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `pwd` varchar(100) DEFAULT NULL,
+  `cpwd` varchar(100) DEFAULT NULL,
+  `pno` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `ureg` */
+
+insert  into `ureg`(`name`,`email`,`pwd`,`cpwd`,`pno`) values ('indira','indira@gmail.com','123',NULL,2147483647),('janu','janu@gmail.com','123',NULL,2147483647),('jahnavi','jahnavi@gmail.com','7410',NULL,2147483647),('q','q@gmail.com','123',NULL,2147483647),('a','a@gmail.com','a',NULL,2147483647);
+
+/*Table structure for table `userlog` */
+
+DROP TABLE IF EXISTS `userlog`;
+
+CREATE TABLE `userlog` (
+  `email` varchar(100) DEFAULT NULL,
+  `pwd` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `userlog` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
